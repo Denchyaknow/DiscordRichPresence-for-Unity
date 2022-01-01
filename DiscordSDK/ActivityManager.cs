82074165d2b4e6cd1ccea43b268a5280c6874acb
Dencho <denchyaknow@gmail.com>
2022-01-01 09:00:39 -0800

    Added the Discord SDK

---
+++ DiscordSDK/ActivityManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yakno.Discord
+{
+    public partial class ActivityManager
+    {
+        public void RegisterCommand()
+        {
+            RegisterCommand(null);
+        }
+    }
+}
